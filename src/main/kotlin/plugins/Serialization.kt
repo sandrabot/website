374056a810f3a70a20eef6eefbb1129e5a1f6151
averen <avery@averen.dev>
2026-03-03 03:32:28 -0700

    install json content negotiation

--- src/main/kotlin/plugins/Serialization.kt
+++ src/main/kotlin/plugins/Serialization.kt
@@ -14,14 +14,19 @@
  * limitations under the License.
  */
 
+package plugins
+
+import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
-import plugins.configureSerialization
-import plugins.configureRouting
+import io.ktor.server.application.install
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import kotlinx.serialization.json.Json
 
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
-
-@Suppress("unused")
-fun Application.module() {
-    configureSerialization()
-    configureRouting()
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+        json(Json {
+            prettyPrint = true
+            isLenient = true
+        })
+    }
 }
